cb1571868df05de85a7fc1c034b2e2cffd9fd6bf
vanya-antonov <vanya.antonov@gmail.com>
2022-03-06 18:14:48 +0300

    Diagram to build COFs

--- src/statistics.R
+++ src/statistics.R
@@ -3,9 +3,13 @@ source("lib.R")
 
 ###
 
-# "First, we analyzed all the genes annotated in 240 phage and 98 Streptomyces genomes"
 orgs_df <- read.delim(paste0(DATA_DIR, "all_orgs.tsv"), as.is=TRUE, na.strings = "NULL") %>%
   filter(num_annotated_genes > 0 )
+
+cofs_df <- read.delim(paste0(DATA_DIR, "all_cofs.tsv"), as.is=TRUE, na.strings = "NULL")
+
+
+# "First, we analyzed all the genes annotated in 240 phage and 98 Streptomyces genomes"
 orgs_df %>% filter(kingdom == 'Bacteria') %>% nrow()
 orgs_df %>% filter(kingdom == 'Viruses') %>% nrow()
 
@@ -19,3 +23,16 @@ bacteria_df <- orgs_df %>% filter(kingdom == 'Bacteria')
 virus_df <- orgs_df %>% filter(kingdom == 'Viruses')
 100 * sum(bacteria_df$num_tta_genes) / sum(bacteria_df$num_annotated_genes)
 100 * sum(virus_df$num_tta_genes) / sum(virus_df$num_annotated_genes)
+
+# "72,744 predicted genes with frameshifts"
+sum(orgs_df$num_fshifts)
+
+# 4,162 clusters (COFs) include 16,092 frameshifted genes
+nrow(cofs_df)
+sum(cofs_df$num_fs)
+
+# 6061 TTA-genes have similarity with 1077 identified COFs
+cofs_df %>% filter(num_tta_genes > 0) %>% pull(num_tta_genes) %>% sum()
+cofs_df %>% filter(num_tta_genes > 0) %>% nrow()
+
+
