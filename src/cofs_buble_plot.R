118f6b6195d285f62a097f928b3dc8d2c664bf18
vanya-antonov <vanya.antonov@gmail.com>
2022-11-29 16:55:25 +0300

    Updated figs before submission

--- src/cofs_buble_plot.R
+++ src/cofs_buble_plot.R
@@ -8,14 +8,17 @@ df <- read.table(input_file, sep="\t", header=TRUE, as.is = TRUE)
 head(df)
 tail(df)
 
+color_v = c('red'='red', 'black'='black')
 
 df %>%
   mutate(
     log_pvalue = -log10(p_value_min),
-    TTA_peak_perc = 100*TTA_peak.num_TTA/Number_all_TTA_genes) %>%
+    TTA_peak_perc = 100*TTA_peak.num_TTA/Number_all_TTA_genes,
+    color = ifelse(FEATURE == 'arac family transcriptional regulator', 'red', 'black')) %>%
   ggplot() +
-  aes(x = Cluster_size, y = log_pvalue, size = TTA_peak_perc) %>%
+  aes(x = Cluster_size, y = log_pvalue, size = TTA_peak_perc, col = color) %>%
   geom_point(alpha=0.45) +
+  scale_color_manual(values=color_v) +
   theme(legend.position="bottom") +
   # Rename legend: http://www.cookbook-r.com/Graphs/Legends_(ggplot2)/
   scale_size_continuous(name  ="% aligned TTA codons: ") +
